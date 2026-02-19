88ce722c0c9d721e3360a1141642601b497d6387
Ivan Nizhnikovskiy <ivan.nizhnikovskiy.dev@gmail.com>
2026-02-19 13:42:30 +0100

    GH pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __dirname = dirname(__filename)
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/medi-drive/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
